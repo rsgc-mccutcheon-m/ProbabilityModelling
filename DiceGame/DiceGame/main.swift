d31f997e8f290b169c478b7bb7d782d26fa80bca
Mark McCutcheon <mark.mccutcheon29@gmail.com>
2017-01-18 12:38:40 -0500

    began building the input sequence

--- DiceGame/DiceGame/main.swift
+++ DiceGame/DiceGame/main.swift
@@ -9,67 +9,8 @@
 import Foundation
 
 
-//MARK: GameBoard Struct
+//MARK: GameBoard Class Init
 
-class gameBoard {
-    
-    var player : String
-    var wins : Int
-    var boardPicks = [Int]() //store 5 chosen game board values to check against
-    var boardStatus = [Bool]() //store the status of the five game spaces
-    var score : Int
-   
-    
-    
-    init (picks : [Int], player : String ) {
-        
-        self.boardPicks = picks //set the player's pick array to the input int array
-
-        
-        //set to the dafault of none true before play
-        self.boardStatus = [
-         false, false, false, false, false
-        ]
-        
-        self.player = player //set player name to the String passed in the init
-        self.wins = 0 //set wins to default of 0 to begin
-        self.score = 0
-        
-    }
-    
-    
-    //run after each roll to check if the player gets to cross off a number, or uncross a number
-     func check(rollVal : Int ) {
-        for (index, pick) in self.boardPicks.enumerated() {
-        
-            if pick == rollVal {
-            self.boardStatus[index] = !self.boardStatus[index]
-        }
-        }
-    }
-    
-    //run at end of round to get a players finishing count
-     func tally() {
-        for (index, pick) in self.boardPicks.enumerated() { //iterate through 5 indicies
-            
-            if self.boardStatus[index] == false { //check if the value at a given index has been crossed out, or uncrossed, or whatnot
-                self.score += pick //if the value is in play, add it to the final tally for that round
-            }
-        }
-    }
-    
-    //run at end of round to re-pick and play again. will need to create a new array of picks to input
-     func refresh(picks : [Int]) {
-        
-        self.score = 0
-        self.boardPicks = []
-        self.boardStatus = [
-            false, false, false, false, false
-        ]
-        
-        self.boardPicks = picks
-    }
-}
 
 
 //MARK: Global Input Variables
@@ -80,28 +21,32 @@ var inPicks : [Int]
 
 var success : Bool
 
+var promptCount = "Please specify the desired number of players as an integer:  "
+
 //MARK: Poll Input
 
 
 success = false //set input iteration control to default of false
 
 repeat {
-    var player
     
+    print(promptCount)
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    success = true
+    guard let playerCount = readLine(strippingNewline: true) else {
+    success = false
+    }
     
 } while (!success)
 
-
+for index in 0...(playerCount-1) {
+    
+    
+    
+    
+    
+    
+}
 
 
 
